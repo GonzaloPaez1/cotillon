8d4d1766ddbd90bb234ee642281cdf7e03c9d95e
Gonzalo Paez <gonzaaapaez@gmail.com>
2022-11-02 10:34:34 -0300

    as

--- TodoSueltoQualgia/js/login.js
+++ TodoSueltoQualgia/js/login.js
@@ -6,16 +6,20 @@ function inicioSesion(usuarios){
     let userFound = usuarios.find((usuario)=>{
         return usuario.username === usuarioReg.value && usuario.password === contraseniaReg.value
     })
-
+let ingreso = false;
+console.log(ingreso);
 if (userFound){
     window.location.href= "./index.html";
-    document.querySelector("#bienvenida").innerText ="Bienvenido"
+    ingreso = true;
 }else{
     document.querySelector("#warnings").innerText = "usuario incorrecto"
+    
+} 
+return ingreso;
 }
-return userFound;
+function ingresoDeTexto(){
+    document.querySelector("#warnings").innerText = "usuario incorrecto"
 }
-
 function recuperarLS(){
     let datos = JSON.parse(localStorage.getItem("usuarios"));
     return datos;
@@ -25,6 +29,7 @@ const usuariosLS = recuperarLS();
 
 btnIngresar.addEventListener("click", (e) =>{
     e.preventDefault();
-    inicioSesion(usuariosLS)
-
+    inicioSesion(usuariosLS);
+    
 });
+
